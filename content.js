50e236fe294ad52b5a49b0e15cd75061e9fdcb9e
rustleupchef <rustle.up.chef@gmail.com>
2025-01-30 21:53:01 -0600

    Made the images center when scrolling through, and added different highlights

--- content.js
+++ content.js
@@ -11,6 +11,9 @@ browser.runtime.onMessage.addListener((message) => {
     }
 
     if (message.type === "MOVE_TO") {
+        imagesList[index].classList.remove('textlens-highlight-selected');
+        imagesList[index].classList.add('textlens-highlight-unselected');
+
         if (message.data === "FORWARDS") {
             if (imagesList.length > 0) {
                 index = (index + 1) % imagesList.length;
@@ -26,7 +29,8 @@ browser.runtime.onMessage.addListener((message) => {
                 }
             }
         }
-        imagesList[index].scrollIntoView({behavior: "smooth", block: "start"});
+        imagesList[index].scrollIntoView({behavior: "smooth", block: "center"});
+        imagesList[index].classList.add('textlens-highlight-selected');
     }
 });
 
@@ -36,7 +40,7 @@ function grabImgs(name) {
     index = 0
     imagesList.forEach(image => {
         if (image !== null) {
-            image.classList.remove('textlens-highlight');
+            image.classList.remove('textlens-highlight-unselected', 'textlens-highlight-selected');
         }
     });
     imagesList = new Array();
@@ -45,13 +49,16 @@ function grabImgs(name) {
     const images = Array.from(imgs);
     images.forEach(image => {
         if (image.hasAttribute('src') && image.src.includes(name)) {
-            image.classList.add("textlens-highlight");
+            image.classList.add("textlens-highlight-unselected");
             imagesList.push(image);
         }
 
         if (image.hasAttribute('alt') && image.alt.includes(name)) {
-            image.classList.add("textlens-highlight");
+            image.classList.add("textlens-highlight-unselected");
             imagesList.push(image);
         }
     });
+    if (imagesList.length > 0) {
+        imagesList[index].classList.add('textlens-highlight-selected');
+    }
 }
